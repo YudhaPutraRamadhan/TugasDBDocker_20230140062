2beb1beaddeb2c7ddd2957ae520804ec641f2fc2
YudhaPutraRamadhan <yudharamadhan838@gmail.com>
2026-04-20 12:04:50 +0700

    Menambahkan repository untuk User

---
+++ src/main/java/com/praktikum_database/deploy/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.praktikum_database.deploy.repository;
+
+
+import com.praktikum_database.deploy.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+}
